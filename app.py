05c494258f05f929855ff5536c47c4fb607e5acb
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-19 20:06:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,6 +151,8 @@ def main():
                   import re # topic subtopic subtopic2
                   from unidecode import unidecode
                   text1 = str(get_text_ending_to_index(raw_text2))
+                  text22 = text1
+                  st.write(text22)
                   text1 = re.sub(r' {2,}', ' ',re.sub(r'\n{2,}', '\n', text1))
                   text1 = re.sub(r'‘', r'', text1)
                   text1 = re.sub(r' \n', '\n',re.sub(r'\n ', '\n', text1)) #works
@@ -167,7 +169,7 @@ def main():
                   text1 = re.sub(r'\n', r'\n\n', text1) #works
                   text1 = re.sub(r'-', r' ',text1)
                   text1 = unidecode(text1)
-                  st.write(text1)
+                  
     
                   import re # topic subtopic subtopic2
                   text2 = str(get_text_starting_from_index(raw_text2))
