b1dc494741f7527d1f27aa5064f87d0c66fc4d42
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-20 01:59:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -419,8 +419,8 @@ def main():
               SentenceTransformerEmbeddings,
               )
               embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-              #from langchain_community.vectorstores import Chroma
-              from langchain_chroma import Chroma
+              from langchain_community.vectorstores import Chroma
+              #from langchain_chroma import Chroma
               db = Chroma.from_documents(documents, embedding_function)
     
               retriever = db.as_retriever()
