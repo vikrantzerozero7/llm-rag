8baa724fcfe7a4c3126bb484e676a441d9a633fb
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-22 13:57:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -443,29 +443,28 @@ def main():
                   | model4
                   | StrOutputParser()
               )
-              upload = "Data Upload successful"
-              if upload == "Data Upload successful":
-                  while True:
-                      query = st.text_input("Ask Question")
-        
-                      # prompt: get question and answer part
-                      if st.button('Submit Answer'):
-                          result = chain.invoke(query)
-                          
-                    
-                          if "answer is not available in the context" in result:
-                              st.write("No answer")
-                          else:
-                              st.write(result)
-                              docs1 = vector_store.similarity_search(query,k=3)
-                              data_dict = docs1[0].metadata
-                              st.write("\nBook Name : ",data_dict["Book name"])
-                              st.write("Chapter : ",data_dict["Chapter"])
-                              st.write("Title : ",data_dict["Topic"])
-                              st.write("Subtopic : ",data_dict["Subtopic"])
-                              st.write("Subsubtopic : ",data_dict["Subsubtopic"])
-                      if st.button('STOP'):
-                          break
+             
+              while True:
+                  
+                  query = st.text_input("Ask Question")
+                  # prompt: get question and answer part
+                  if st.button('Submit Answer'):
+                      
+                      result = chain.invoke(query)
+                
+                      if "answer is not available in the context" in result:
+                          st.write("No answer")
+                      else:
+                          st.write(result)
+                          docs1 = vector_store.similarity_search(query,k=3)
+                          data_dict = docs1[0].metadata
+                          st.write("\nBook Name : ",data_dict["Book name"])
+                          st.write("Chapter : ",data_dict["Chapter"])
+                          st.write("Title : ",data_dict["Topic"])
+                          st.write("Subtopic : ",data_dict["Subtopic"])
+                          st.write("Subsubtopic : ",data_dict["Subsubtopic"])
+                  if st.button('STOP'):
+                      break
 
 if __name__=='__main__':
     main()
