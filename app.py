72edf14e5997a20f1071a3c83874ca14d7a05714
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-24 01:18:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -438,21 +438,20 @@ def main():
          st.sidebar.warning("you need to upload a pdf file.")
         
     query = st.text_input("Enter query",placeholder="text") 
-    if st.button('Submit query'):
-        
-        result1,vector_store1 = chain_result(pdf_d,query)
-        
-        if "answer is not available in the context" in result1:
-              st.write("No answer") 
-        else:
-              st.write(result1)
-              docs1 = vector_store1.similarity_search(query,k=3)
-              data_dict = docs1[0].metadata
-              st.write("\nBook Name : ",data_dict["Book name"])
-              st.write("Chapter : ",data_dict["Chapter"])
-              st.write("Title : ",data_dict["Topic"])
-              st.write("Subtopic : ",data_dict["Subtopic"])
-              st.write("Subsubtopic : ",data_dict["Subsubtopic"])
+   
+    result1,vector_store1 = chain_result(pdf_d,query)
+    
+    if "answer is not available in the context" in result1:
+          st.write("No answer") 
+    else:
+          st.write(result1)
+          docs1 = vector_store1.similarity_search(query,k=3)
+          data_dict = docs1[0].metadata
+          st.write("\nBook Name : ",data_dict["Book name"])
+          st.write("Chapter : ",data_dict["Chapter"])
+          st.write("Title : ",data_dict["Topic"])
+          st.write("Subtopic : ",data_dict["Subtopic"])
+          st.write("Subsubtopic : ",data_dict["Subsubtopic"])
     
 if __name__=='__main__':
     main()
