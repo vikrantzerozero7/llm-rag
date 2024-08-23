ea4d8c3702f5bd601ecff885e0f473c1aa92cefe
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-24 01:10:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -424,43 +424,36 @@ def main():
     
     # Store the initial value of widgets in session state
    
-    uploadedFile = st.sidebar.file_uploader("Choose a file" ,accept_multiple_files=True,key="fileUploader",type = "pdf")
+    # File uploader to select multiple PDF files
+    uploaded_files = st.sidebar.file_uploader("Choose a file", accept_multiple_files=True, key="fileUploader", type="pdf")
     
-    if uploadedFile is not None :
-        try:
-            uploadedFile1=uploadedFile.getvalue()
-            
-            df=fitz.open(stream=uploadedFile1, filetype="pdf")
-        except:
-                try: 
-                    uploadedFile1=uploadedFile.getvalue()
-                    df = fitz.open(stream=uploadedFile1, filetype="pdf")
-
-                except:
-                    uploadedFile1 = uploadedFile.getvalue()
-                    df = fitz.open(stream=uploadedFile1, filetype="pdf")
-
+    # Initialize an empty list to store the opened PDF documents
+    pdf_d = []
     
-        if df is not None :
-            pdf_d.append(df)
-        query = st.text_input("Enter query",placeholder="text") 
-        if st.button('Submit query'):
-            
-            result1,vector_store1 = chain_result(pdf_d,query)
-            
-            if "answer is not available in the context" in result1:
-                  st.write("No answer") 
-            else:
-                  st.write(result1)
-                  docs1 = vector_store1.similarity_search(query,k=3)
-                  data_dict = docs1[0].metadata
-                  st.write("\nBook Name : ",data_dict["Book name"])
-                  st.write("Chapter : ",data_dict["Chapter"])
-                  st.write("Title : ",data_dict["Topic"])
-                  st.write("Subtopic : ",data_dict["Subtopic"])
-                  st.write("Subsubtopic : ",data_dict["Subsubtopic"])
+    if uploaded_files is not None:
+        for uploaded_file in uploaded_files:
+            df = fitz.open(stream=uploaded_file.read(), filetype="pdf")  # Open the uploaded PDF file
+            pdf_d.append(df)  # Add the opened PDF document to the list
     else:
          st.sidebar.warning("you need to upload a pdf file.")
+        
+    query = st.text_input("Enter query",placeholder="text") 
+    if st.button('Submit query'):
+        
+        result1,vector_store1 = chain_result(pdf_d,query)
+        
+        if "answer is not available in the context" in result1:
+              st.write("No answer") 
+        else:
+              st.write(result1)
+              docs1 = vector_store1.similarity_search(query,k=3)
+              data_dict = docs1[0].metadata
+              st.write("\nBook Name : ",data_dict["Book name"])
+              st.write("Chapter : ",data_dict["Chapter"])
+              st.write("Title : ",data_dict["Topic"])
+              st.write("Subtopic : ",data_dict["Subtopic"])
+              st.write("Subsubtopic : ",data_dict["Subsubtopic"])
+    
 if __name__=='__main__':
     main()
 
