bf301e7ce950316e635df5ba0f18ce293ee08edb
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-26 17:59:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -428,7 +428,7 @@ def main():
                     st.sidebar.write("File processed successfully")
                     result1 =  st.session_state.chain.invoke(st.session_state.query) 
                     
-                    if "does not provide any information" in result1 or "does not contain any information" in result1 or "answer is not available" in result1:
+                    if "ujnjkn" in result1:#"does not provide any information" in result1 or "does not contain any information" in result1 or "answer is not available" in result1:
                           st.write("No answer") 
                     else:
                           st.write(result1)
