34025afb350fa9a63bc1af0397ba20ed452eb703
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-26 19:56:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -336,7 +336,7 @@ def chain_result(pdf_d):
         Context:\n {context}?\n
         Question: \n{question}\n
         Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-        provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+        provided context just say, "answer is not available in the context",strictly be specific, don't provide the wrong answer\n\n
         
     Answer:
     """
