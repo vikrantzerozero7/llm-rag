b96f848408d8b91f3df7eb71b0ef88fa8e87d3d0
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-26 18:13:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -334,7 +334,7 @@ def chain_result(pdf_d):
 
       prompt_template = """
         Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-        provided context specifically say, "answer is not available in the context", don't provide the wrong answer, be specific, write "REMOVE66" if  if the answer is not in
+        provided context specifically say, "REMOVE66", don't provide the wrong answer, be specific, write "REMOVE66" if the answer is not in
         provided context  \n\n
         Context:\n {context}?\n
         Question: \n{question}\n
