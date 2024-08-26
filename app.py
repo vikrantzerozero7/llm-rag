9e8d50aa5f5c17d19e2b808d0d81bf3815b0e22d
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-26 16:13:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -413,41 +413,37 @@ def main():
             else:
                 st.write("") 
         else:
-            st.session_state.bool = False
-        
-    # Check if pdf_d is already in session state, if not, initialize it
-    query = st.text_input("Ask query and press enter",placeholder="Ask query and press enter",key = "key")
-    st.session_state.query = query
-    time.sleep(1)
-    if st.button("Submit"):
-        if uploaded_files:
-            
-            if st.session_state.bool==True:
-                st.sidebar.write("File processed successfully")
-                result1 =  st.session_state.chain.invoke(st.session_state.query) 
-                
-                if "does not provide any information" in result1 or "does not contain any information" in result1 or "answer is not available" in result1:
-                      st.write("No answer") 
-                else:
-                      st.write(result1)
-                      docs1 =  st.session_state.vector_store1.similarity_search( query,k=3)
-                      data_dict = docs1[0].metadata
-                      st.write("\nBook Name : ",data_dict["Book name"])
-                      st.write("Chapter : ",data_dict["Chapter"])
-                      st.write("Title : ",data_dict["Topic"])
-                      st.write("Subtopic : ",data_dict["Subtopic"])
-                      st.write("Subsubtopic : ",data_dict["Subsubtopic"])
+            # Check if pdf_d is already in session state, if not, initialize it
+            query = st.text_input("Ask query and press enter",placeholder="Ask query and press enter",key = "key")
+            st.session_state.query = query
+            time.sleep(1)
+            if st.button("Submit"):
+                if uploaded_files:
+                    if "bool" in st.session_state:
+                        if st.session_state.bool==True:
+                            st.sidebar.write("File processed successfully")
+                            result1 =  st.session_state.chain.invoke(st.session_state.query) 
+                            
+                            if "does not provide any information" in result1 or "does not contain any information" in result1 or "answer is not available" in result1:
+                                  st.write("No answer") 
+                            else:
+                                  st.write(result1)
+                                  docs1 =  st.session_state.vector_store1.similarity_search( query,k=3)
+                                  data_dict = docs1[0].metadata
+                                  st.write("\nBook Name : ",data_dict["Book name"])
+                                  st.write("Chapter : ",data_dict["Chapter"])
+                                  st.write("Title : ",data_dict["Topic"])
+                                  st.write("Subtopic : ",data_dict["Subtopic"])
+                                  st.write("Subsubtopic : ",data_dict["Subsubtopic"])
+                        else:
+                             st.write("Process file/files first")
+                    else:
+                        st.write("Process file/files first")
+                else: 
+                    st.write("Upload and process file/files first")
             else:
-                 st.write("Process file/files first") 
-        else: 
-            st.write("Upload and process file/files first")
-    else:
-        st.write("")
-
-   
-    
-                
-            
+                st.write("")
+       
 if __name__=='__main__':
     main()
     
