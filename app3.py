eddc642338ce66c21564d474cf5cce3f4c51f279
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-09-01 01:39:05 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -159,54 +159,55 @@ def main():
   
     st.session_state.query = query
 
-    new_data1 = {
-        'Query': query
-    }
-    new_data = pd.DataFrame([new_data1])
-
-    # Username of your GitHub account
-    g = Github("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")
-    user = g.get_user()
-    repository = user.get_repo('llm-rag')
-
-    # Get the file content from the repository
-    file_content = repository.get_contents('file1.csv')
-    bytes_data = file_content.decoded_content
-    s = str(bytes_data, 'utf-8')
-
-    # Write the file content to a local file
-    with open("data.txt", "w") as file:
-        file.write(s)
-
-    df11 = pd.read_csv('data.txt')
-    csv = df11
-    csv2 = new_data
-    csv3 = pd.concat([csv, csv2], axis=0)
-    csv3 = csv3.reset_index(drop=True)
-
-    dataset = csv3.drop_duplicates()
-
-    # Upload data to GitHub
-    df2 = dataset.to_csv(sep=',', index=False)
-    file_list = [df2]
-    file_names = ['file1.csv']
-
-    commit_message = "Data Updated - " + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    repo = g.get_user().get_repo('llm-rag')
-    master_ref = repo.get_git_ref("heads/main")
-    master_sha = master_ref.object.sha
-    base_tree = repo.get_git_tree(master_sha)
-    element_list = []
-    for i in range(len(file_list)):
-        element = InputGitTreeElement(file_names[i], '100644', 'blob', file_list[i])
-        element_list.append(element)
-    tree = repo.create_git_tree(element_list, base_tree)
-    parent = repo.get_git_commit(master_sha) 
-    commit = repo.create_git_commit(commit_message, tree, [parent])
-    master_ref.edit(commit.sha)
+    
     
     time.sleep(1)
     if st.button("Submit"):
+            new_data1 = {
+            'Query': query
+        }
+        new_data = pd.DataFrame([new_data1])
+    
+        # Username of your GitHub account
+        g = Github("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")
+        user = g.get_user()
+        repository = user.get_repo('llm-rag')
+    
+        # Get the file content from the repository
+        file_content = repository.get_contents('file1.csv')
+        bytes_data = file_content.decoded_content
+        s = str(bytes_data, 'utf-8')
+    
+        # Write the file content to a local file
+        with open("data.txt", "w") as file:
+            file.write(s)
+    
+        df11 = pd.read_csv('data.txt')
+        csv = df11
+        csv2 = new_data
+        csv3 = pd.concat([csv, csv2], axis=0)
+        csv3 = csv3.reset_index(drop=True)
+    
+        dataset = csv3.drop_duplicates()
+    
+        # Upload data to GitHub
+        df2 = dataset.to_csv(sep=',', index=False)
+        file_list = [df2]
+        file_names = ['file1.csv']
+    
+        commit_message = "Data Updated - " + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        repo = g.get_user().get_repo('llm-rag')
+        master_ref = repo.get_git_ref("heads/main")
+        master_sha = master_ref.object.sha
+        base_tree = repo.get_git_tree(master_sha)
+        element_list = []
+        for i in range(len(file_list)):
+            element = InputGitTreeElement(file_names[i], '100644', 'blob', file_list[i])
+            element_list.append(element)
+        tree = repo.create_git_tree(element_list, base_tree)
+        parent = repo.get_git_commit(master_sha) 
+        commit = repo.create_git_commit(commit_message, tree, [parent])
+        master_ref.edit(commit.sha)
         if uploaded_files:
             if "bool" in st.session_state:
                 if st.session_state.bool==True:
