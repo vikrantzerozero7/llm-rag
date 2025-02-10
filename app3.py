922c75f2769c02cda08cac9e9d4f2d38c161661a
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2025-02-10 17:32:09 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-uploaded_file = st.file_uploader("Upload a file")
+uploaded_file = st.sidebar.file_uploader("Upload a file")
 
 if uploaded_file:
    st.write("Filename: ", uploaded_file.name)
