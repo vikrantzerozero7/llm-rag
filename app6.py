a917bcaa98a53609adebf31a71def2a53f0f2200
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2025-03-15 19:48:52 +0530

    Update app6.py

--- app6.py
+++ app6.py
@@ -213,7 +213,7 @@ def main():
                         #st.session_state.pdf_d.append(df)  
                     
                     data_data2 = chain_result(st.session_state.pdf_d)
-                    if data_data2 == "New data uploaded"
+                    if data_data2 == "New data uploaded":
                         st.write("New data added")
                     elif data_data2 == "Data is already there":
                       st.write("Data is already there")
