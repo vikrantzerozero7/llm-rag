5bd62375192bf74d9ea4c17d959b19f68945ab97
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2025-03-16 11:56:42 +0530

    Update app6.py

--- app6.py
+++ app6.py
@@ -75,7 +75,7 @@ def chain_result(pdf_d):
     
     # ✅ Get existing indexes properly
     existing_indexes = IndexFactory.list()  # Returns a list of IndexModel objects
-    existing_index_names = [idx.name for idx in existing_indexes]  # Extract names
+    existing_index_names = [idx for idx in existing_indexes]  # Extract names
 
     for pdf in pdf_d:
         # Step 1: Open PDF
