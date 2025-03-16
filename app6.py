9e6a002b7239aadaaf78f1af80a9f1555524c469
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2025-03-16 12:53:32 +0530

    Update app6.py

--- app6.py
+++ app6.py
@@ -42,8 +42,8 @@ def result(query):
     # Print the search results
     #print(json.dumps(response.details, indent=4)) 
     import ast
-    st.write(results)
-    #st.write(ast.literal_eval(documents2[0]["id"])['Book file'])
+    #st.write(results)
+    st.write(ast.literal_eval(documents2[0]["documents"])['Book file'])
     return response1["data"]
 
     
