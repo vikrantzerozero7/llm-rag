9566220faf78c372e4bf6cf47b683223a41485fb
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2025-03-17 16:05:22 +0530

    Update app6.py

--- app6.py
+++ app6.py
@@ -284,7 +284,7 @@ def main():
                 else: 
                     st.write("Upload and process file/files first")
     else:
-        st.write("")
+        pass
 
 if __name__=='__main__':
     main()
